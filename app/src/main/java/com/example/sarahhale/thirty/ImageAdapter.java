0632969cf89dd543dba9ccb27b79316e037005ae
Sarah <sarahhale95@gmail.com>
2018-06-08 16:45:46 +0200

    can set dice to active and inactive

--- app/src/main/java/com/example/sarahhale/thirty/ImageAdapter.java
+++ app/src/main/java/com/example/sarahhale/thirty/ImageAdapter.java
@@ -12,31 +12,16 @@ public class ImageAdapter extends BaseAdapter {
     private Context mContext;
     private Dice dice;
 
-    // references to our active images
-    private Integer[] activeImages = {
-            R.drawable.white1, R.drawable.white2,
-            R.drawable.white3, R.drawable.white4,
-            R.drawable.white5, R.drawable.white6
-    };
-
-    // references to our inactive images
-    private Integer[] inActiveImages = {
-            R.drawable.grey1, R.drawable.grey2,
-            R.drawable.grey3, R.drawable.grey4,
-            R.drawable.grey5, R.drawable.grey6
-    };
 
     public ImageAdapter(Context c, Dice dice) {
         mContext = c;
         this.dice = dice;
-      /*  for (int i = 0; i <dice.size(); i++){
-            activeImages[i] = setImage(dice.get(i).getValue(),dice.get(i).isActive());
-        }*/
+
     }
 
 
     public int getCount() {
-        return activeImages.length;
+        return 6;
     }
 
     public Object getItem(int position) {
@@ -59,12 +44,12 @@ public class ImageAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-        System.out.println(position);
+
         imageView.setImageResource(setImage(dice.getDice().get(position).getValue(),dice.getDice().get(position).isActive() ));
         return imageView;
     }
 
-    public int setImage (int number,boolean active) {
+   static public int setImage (int number,boolean active) {
         switch (number) {
             case 1:
                 if(active) return R.drawable.white1;
--- app/src/main/java/com/example/sarahhale/thirty/MainActivity.java
+++ app/src/main/java/com/example/sarahhale/thirty/MainActivity.java
@@ -2,24 +2,59 @@ package com.example.sarahhale.thirty;
 import com.example.sarahhale.thirty.playlogic.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private Dice dice;
+    GridView gridView;
+    Button rollButton;
+    ImageAdapter imageAdapter;
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Dice dice = new Dice(6);
-        GridView gridView = (GridView) findViewById(R.id.gridview);
+        
+        dice = new Dice(6);
+        gridView = (GridView) findViewById(R.id.gridview);
+        rollButton = (Button) findViewById(R.id.rollButton);
+        imageAdapter =new ImageAdapter(this,dice);
 
         // Instance of ImageAdapter Class
-        gridView.setAdapter(new ImageAdapter(this,dice));
+        gridView.setAdapter(imageAdapter);
+
+        /**
+         * Change image after image being clicked.
+         * */
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                if(dice.getDice().get(position).isActive()){
+                    dice.getDice().get(position).setInactive();
+                }else{
+                    dice.getDice().get(position).setActive();
+                }
+                ((ImageView) v).setImageResource(
+                        ImageAdapter.setImage(
+                                dice.getDice().get(position).getValue(),
+                                dice.getDice().get(position).isActive()
+                        ));
+
+            }
+        });
 
     }
 
+    public void rollDice(View view){
+        dice.rollAllDice();
+        imageAdapter.notifyDataSetChanged();
+    }
 
 }
 
